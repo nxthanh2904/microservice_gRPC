f370ef48fcba4817a08d6eeda6eeabd39a738d42
thanhnx1vist <nxthanh2804@gmail.com>
2020-06-25 11:23:24 +0700

    aa

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 const grpc = require('grpc');
-const  PROTO_PATH = 'notes.proto'
+const  PROTO_PATH = './notes.proto'
 const NoteService = grpc.load(PROTO_PATH).NoteService;
 const client = new NoteService('localhost:50051',
             grpc.credentials.createInsecure());
